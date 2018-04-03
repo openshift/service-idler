20407371142f4539cfd22ac4080dc721b379746d
Solly Ross <sross@redhat.com>
2018-04-03 15:01:23 -0400

    Add License and Boilerplate
    This adds in the license and boilerplate license header for the Apache
    License 2.0.

--- pkg/inject/zz_generated.kubebuilder.go
+++ pkg/inject/zz_generated.kubebuilder.go
@@ -1,4 +1,18 @@
-// TODO: add apache license boilerplate here
+/*
+Copyright 2018 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package inject
 
 import (
@@ -50,17 +64,6 @@ func init() {
 			"",
 		},
 		Resources: []string{
-			"endpoints",
-		},
-		Verbs: []string{
-			"get", "list", "watch",
-		},
-	})
-	Injector.PolicyRules = append(Injector.PolicyRules, rbacv1.PolicyRule{
-		APIGroups: []string{
-			"",
-		},
-		Resources: []string{
 			"events",
 		},
 		Verbs: []string{
@@ -76,6 +79,17 @@ func init() {
 		},
 		Verbs: []string{
 			"get", "update",
+		},
+	})
+	Injector.PolicyRules = append(Injector.PolicyRules, rbacv1.PolicyRule{
+		APIGroups: []string{
+			"",
+		},
+		Resources: []string{
+			"endpoints",
+		},
+		Verbs: []string{
+			"get", "list", "watch",
 		},
 	})
 	// Inject GroupVersions
